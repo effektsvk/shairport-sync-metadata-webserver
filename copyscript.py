2a4f351b8c9dd605498bc46e61117af886d3be9e
Erik Slovak <erik.slovak7@gmail.com>
2016-04-24 20:28:06 +0200

    version 0.2 -- working

--- copyscript.py
+++ copyscript.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import os
 import time
+from shutil import copyfile
 while True:
         if (os.path.getsize("/tmp/shairport-sync/image") == os.path.getsize("/root/metadata_webserver/shairport-sync-metadata-webserver/image")) == True:
                 time.sleep( 5 )
                 print("Now playing album art equals to the one on webserver.")
                 continue
         else:
-                shutil.copyfile('/tmp/shairport-sync/image', '/root/metadata_webserver/shairport-sync-metadata-webserver/image')
+                copyfile('/tmp/shairport-sync/image', '/root/metadata_webserver/shairport-sync-metadata-webserver/image')
                 print("Copying from tmp to webserver...")
                 continue
