f3053363d1dcd4b39b2d6ee74ddbc521659a5972
Erik Slovak <erik.slovak7@gmail.com>
2016-04-25 00:00:37 +0200

    version 1.1 -- slightly improved timing

--- copyscript.py
+++ copyscript.py
@@ -8,21 +8,21 @@ while True:
                 copyfile('/tmp/shairport-sync/image', '/root/shairport-sync-metadata-webserver/image')
                 while True:
                         if (os.path.getsize("/tmp/shairport-sync/image") == os.path.getsize("/root/shairport-sync-metadata-webserver/image")) == True:
-                                time.sleep( 5 )
+                                time.sleep(1)
                                 print("Now playing album art equals to the one on webserver.")
                                 x = x + 1
-                                while x == 5:
+                                while x == 10:
                                         os.system("./b64_to_image.py image")
                                         x = 1
                                 break
                         else:
                                 copyfile('/tmp/shairport-sync/image', '/root/shairport-sync-metadata-webserver/image')
                                 print("Copying from tmp to webserver...")
-                                time.sleep( 2 )
+                                time.sleep(1)
                                 print("Converting base64 to image...")
                                 os.system("./b64_to_image.py image")
                                 break
         else:
                 print("Music is not playing.")
-                time.sleep( 10 )
+                time.sleep(5)
                 continue
