0d79a02cefc0c5753cad54c78aca27e7e2a0bedd
Erik Slovak <erik.slovak7@gmail.com>
2016-04-24 23:50:12 +0200

    version 1.0 -- fully working version with standby mode

--- copyscript.py
+++ copyscript.py
@@ -2,6 +2,7 @@
 import os
 import time
 from shutil import copyfile
+x = 1
 while True:
         if os.path.exists("/tmp/shairport-sync/image") == True:
                 copyfile('/tmp/shairport-sync/image', '/root/shairport-sync-metadata-webserver/image')
@@ -9,7 +10,11 @@ while True:
                         if (os.path.getsize("/tmp/shairport-sync/image") == os.path.getsize("/root/shairport-sync-metadata-webserver/image")) == True:
                                 time.sleep( 5 )
                                 print("Now playing album art equals to the one on webserver.")
-                                continue
+                                x = x + 1
+                                while x == 5:
+                                        os.system("./b64_to_image.py image")
+                                        x = 1
+                                break
                         else:
                                 copyfile('/tmp/shairport-sync/image', '/root/shairport-sync-metadata-webserver/image')
                                 print("Copying from tmp to webserver...")
