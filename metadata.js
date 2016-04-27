2f7a17e072913c3896adbd0690b7ba4a5d7cc2b2
Erik Slovak <erik.slovak7@gmail.com>
2016-04-27 13:29:13 +0200

    WORKING -- automatic reload when title is changed

--- metadata.js
+++ metadata.js
@@ -16,6 +16,27 @@
         return true;
     }
 }
+	
+	function checkTitle(){
+		titlemetadata = $.ajax({
+			type: "GET",
+			url: "title",
+			async: false,
+			cache: false,
+			success: function(data) {}
+		}).responseText;
+	}
+
+	// function checkifchanged() {
+	
+	// 	if (titlemetadata == titlevar) {
+	// 		titlemetadata = $.ajax({type: "GET", url: "title", async: false}).responseText;
+	// 		if (titlemetadata !== titlevar) {
+	// 		location.reload();
+	// 		clearInterval(interval);
+	// 		}
+	// 	}
+	// };
 
 // while (true) {
 
@@ -24,22 +45,26 @@
 	}
 	else {
 		artistmetadata = $.ajax({type: "GET", url: "artist", async: false}).responseText;
-	}
+	};
 
 	if (doesFileExist("album") == false) {
 		albummetadata = "No Album";
 	}
 	else {
 		albummetadata = $.ajax({type: "GET", url: "album", async: false}).responseText;
-	}
+	};
 
 	if (doesFileExist("title") == false) {
 		titlemetadata = "No Title";
 	}
 	else {
 		titlemetadata = $.ajax({type: "GET", url: "title", async: false}).responseText;
-	}
+	};
+
+	// var interval = setInterval(checkifchanged, 1000);
 // }
+	
+
 	console.log(artistmetadata);
 	console.log(albummetadata);
 	console.log(titlemetadata);
